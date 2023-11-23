79b6f8d30472ccf94bccbc334c44222f392e1b2d
Hamit98 <150825664+Hamit98@users.noreply.github.com>
2023-11-23 21:48:22 +0600

    Delete Уроки directory

--- Уроки/Lists.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    a = [1, 5, 10, 15, 20]
-    result = 0
-    for i in range(len(a)):
-        result = result + a[i]
-        print(f"index: {i},value {a[i]}")
-    print(f"result: {result}")
--- Уроки/lesson11112023.py
+++
@@ -1,8 +0,0 @@
-class SubjectGrade:
-    def __init__(self, name, grade):
-        self.name= name
-        self.grade = grade
-
-
-math = SubjectGrade("Hamit", 78)
-geog = SubjectGrade("Hamit", 110)
