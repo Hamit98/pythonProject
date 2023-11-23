5b250d097db77f599988a95641390cf4c511c47d
Hamit98 <150825664+Hamit98@users.noreply.github.com>
2023-11-23 21:48:45 +0600

    Delete Домашние_задания directory

--- Домашние_задания/HW 11.1.py
+++
@@ -1,5 +0,0 @@
-words = ["европа", "анаконда", "ель", "трассировка", "якудза", "единство"]
-
-sorted_words = sorted(map(lambda word: word.upper(), filter(lambda word: 'е' in word, words)))
-
-print(sorted_words)
--- Домашние_задания/table_list.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-
- for i in range(1,10):
-         for j in range (1, 10):
-                 print(i, "*", j, "=", i * j)
-         print('\n')
